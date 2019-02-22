28fcf04ce0e0b733a8d42cefa747433173028006
mwanginyambura <mwanginyambura@gmail.com>
2019-02-22 08:52:13 -0500

    Add styles in the heading

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-$(#"quiz-form").on("submit", function(event) {
-  event.preventDefault();
-  var $answer = $("#quiz-answer");
-  var answer = $answer.val();
-  conole.log(answer);
-  if (answer === "Open an existing database"){
-    $("#result").text("excelent");
-  } else if {
-    $("#result").text("Try again!");
-  }
+// $(#"quiz-form").on("submit", function(event) {
+//   event.preventDefault();
+//   var $answer = $("#quiz-answer");
+//   var answer = $answer.val();
+//   conole.log(answer);
+//   if (answer === "O pen an existing database"){
+//     $("#result").text("excelent");
+//   } else if {
+//     $("#result").text("Try again!");
+//   }
