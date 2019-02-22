96dfb98d8396d6485136b6defea273d6a59c4253
mwanginyambura <mwanginyambura@gmail.com>
2019-02-22 08:45:10 -0500

    Remove select method

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(#"quiz-form").on("submit", function(event) {
+  event.preventDefault();
+  var $answer = $("#quiz-answer");
+  var answer = $answer.val();
+  conole.log(answer);
+  if (answer === "Open an existing database"){
+    $("#result").text("excelent");
+  } else if {
+    $("#result").text("Try again!");
+  }
