53c34bd97d4cf5a07c8289c0f36db86749911b69
mwanginyambura <mwanginyambura@gmail.com>
2019-02-22 11:31:13 -0500

    correct the %var

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-// $(#"quiz-form").on("submit", function(event) {
-//   event.preventDefault();
-//   var $answer = $("#quiz-answer");
-//   var answer = $answer.val();
-//   conole.log(answer);
-//   if (answer === "O pen an existing database"){
-//     $("#result").text("excelent");
-//   } else if {
-//     $("#result").text("Try again!");
-//   }
+$(document).ready(function(){
+$("form#qz").submit(function(event) {
+   event.preventDefault();
+   var q1 =parseInt($("input:radio[name=qz1]:checked").val());
+   var q2 =parseInt($("input:radio[name=qz2]:checked").val());
+   var q3 =parseInt($("input:radio[name=qz3]:checked").val());
+   var q4 =parseInt($("input:radio[name=qz4]:checked").val());
+   var q5 =parseInt($("input:radio[name=qz5]:checked").val());
+   var q6 =parseInt($("input:radio[name=qz6]:checked").val());
+   var q7 =parseInt($("input:radio[name=qz7]:checked").val());
+     var total =(q1+q2+q3+q4+q5+q6+q7);
+     var percent =((total/40)*100);
+        $("#result").text("you have scored:" +percent+"%");
+});
+});
