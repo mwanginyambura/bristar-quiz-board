b679cfb88f67d14e8a9b3e493fc3a52d1c734f5d
mwanginyambura <mwanginyambura@gmail.com>
2019-02-25 04:34:57 -0500

    created function

--- js/scripts.js
+++ js/scripts.js
@@ -11,27 +11,18 @@ $(document).ready(function() {
     var total = (qz1 + qz2 + qz3 + qz4 + qz5 + qz6 + qz7);
     var percent = ((total / 40) * 100);
     $("#result").text("you have scored:" + percent + "%");
-  });
+  if (percent > 79) {
+    $("#result").text("you have scored:" + percent + "%" + " KUDOOS");
+    $("#result").slideUp();
+  } else if (percent > 59 && percent < 79) {
+    $("#result").text("you have scored:" + percent + "%" + " FAIR");
+    $("#result").slideToggle();
+  } else if (percent > 39 && percent < 59) {
+    $("#result").text("you have scored:" + percent + "%" + " PULL UP YOUR SOCKS");
+    $("#result").slideToggle();
+  } else {
+    $("#result").text("you have scored:" + percent + "%" + " FAIL");
+    $("#result").slideToggle();
+  };
 });
-function getResult(sum) {
- if (percent < 40){
-   result = 'You have failed!';
-   return result
- } else if ( percent > 40 && percent < 80) {
-   result = 'you have fairly passed!';
-   return result
- } else (){
-   result = 'congratualtions you have passed the test!';
-   return result
- }
-}
-
-/* displayResult = getResult(sum1);
-
-$("#display").text(displayResult);
- });
-
-$('#restart').click(function() {
-   location.reload();
- });
-}); */
+});
